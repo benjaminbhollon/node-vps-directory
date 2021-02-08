540b7b5f57af807069961aa3801b3e5344cca176
Benjamin Hollon <benjaminbhollon@gmail.com>
2021-02-08 17:50:16 +0800

    Allow www

--- index.js
+++ index.js
@@ -6,10 +6,12 @@ const directory = require('./directory.json');
 var server = bouncy(function (req, res, bounce) {
   if (directory[req.headers.host] !== undefined) {
     bounce(directory[req.headers.host]);
+  } else if (directory[req.headers.host.slice(4)] !== undefined) {
+    bounce(directory[req.headers.host.slice(4));
   } else {
     res.statusCode = 404;
+    console.log(req.headers.host);
     res.end('no such host');
   }
-  console.log(req.headers.host);
 });
 server.listen(80);
