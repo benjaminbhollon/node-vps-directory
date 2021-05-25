53219c106fbf460d85414843f0ce380f919ae5d3
Benjamin Hollon <benjaminbhollon@gmail.com>
2021-05-26 04:09:44 +0800

    Support HTTPS redirects

--- index.js
+++ index.js
@@ -3,18 +3,29 @@ const bouncy = require('bouncy');
 //Import directory
 const directory = require('./directory.json');
 
-var server = bouncy(function (req, res, bounce) {
-  if (directory[req.headers.host] !== undefined) {
-    bounce(directory[req.headers.host]);
-  } else if (directory[req.headers.host.slice(4)] !== undefined) {
-    res.writeHead(301, {
-      Location: '//' + req.headers.host.replace('www.', '') + req.url
-    });
-    res.end();
-  } else {
-    res.statusCode = 404;
-    res.end('no such host');
-    console.log(req.headers.host);
+function createHandler(protocol) {
+  return (req, res, bounce) => {
+    if (directory[req.headers.host] !== undefined) {
+      if (protocol === 'http' && String(directory[req.headers.host])[0])
+        return res.writeHead(301, {
+          Location: 'https://' + req.headers.host + req.url
+        });
+      bounce(directory[req.headers.host]);
+    } else if (directory[req.headers.host.slice(4)] !== undefined) {
+      res.writeHead(301, {
+        Location: '//' + req.headers.host.replace('www.', '') + req.url
+      });
+      res.end();
+    } else {
+      res.statusCode = 404;
+      res.end('no such host');
+      console.log(req.headers.host);
+    }
   }
-});
-server.listen(80);
+}
+
+const http = bouncy(createHandler('http'));
+http.listen(80);
+
+const https = bouncy(createHandler('https'));
+https.listen(443);
