38aa334fa20166d4548cf3fa1f041553353063db
Benjamin Hollon <benjaminbhollon@gmail.com>
2021-05-26 13:43:31 +0800

    Native SSL support

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 const bouncy = require('bouncy');
+const config = require('./config.json');
+const fs = require('fs');
+const ssl = (config.ssl ? {
+  key: fs.readFileSync(config.ssl.key),
+  cert: fs.readFileSync(config.ssl.cert)
+} : false);
 
 //Import directory
 const directory = require('./directory.json');
@@ -6,10 +12,6 @@ const directory = require('./directory.json');
 function createHandler(protocol) {
   return (req, res, bounce) => {
     if (directory[req.headers.host] !== undefined) {
-      if (protocol === 'http' && String(directory[req.headers.host])[0] === '4')
-        return res.writeHead(301, {
-          Location: 'https://' + req.headers.host + req.url
-        });
       bounce(directory[req.headers.host]);
     } else if (directory[req.headers.host.slice(4)] !== undefined) {
       res.writeHead(301, {
@@ -19,7 +21,7 @@ function createHandler(protocol) {
     } else {
       res.statusCode = 404;
       res.end('no such host');
-      console.log(req.headers.host);
+      console.log('could not find host ' + req.headers.host);
     }
   }
 }
@@ -27,5 +29,7 @@ function createHandler(protocol) {
 const http = bouncy(createHandler('http'));
 http.listen(80);
 
-const https = bouncy(createHandler('https'));
-https.listen(443);
+if (ssl) {
+  const https = bouncy(ssl, createHandler('https'));
+  https.listen(443);
+}
